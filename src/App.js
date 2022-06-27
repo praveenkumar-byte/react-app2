dc2d9fbf096c5afb1d3f33e321f632b6ade0cfd1
praveenkumar-byte <praveenkumar6022@gmail.com>
2022-06-27 23:25:51 +0530

    ad

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Paragraph from "./component/Paragraph";
+import './App.css';
+
+function App() {
+  return (
+   <Paragraph/>
+  );
+}
+
+export default App;
---
+++ src/component/Paragraph.jsx
@@ -0,0 +1,5 @@
+export default function Paragraph(){
+    return(
+        <p>"I am learning React. My life is getting better. "</p>
+    )
+}
